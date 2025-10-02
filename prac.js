a182d80c394426aacc3c42c5a4b6cac64065d84b
Omkar <omkarjoshiop@gmail.com>
2025-10-02 17:19:40 +0530

    The localstorage and json methods of obj convert

--- prac.js
+++ prac.js
@@ -1,3 +1,26 @@
-wndlawndwddadwa
-awdawdawdaw
-dawdamlkndaww
+//obj pratice
+
+let obj={
+    name:'HONDA',
+    average:25,
+    fucntion:function fun()
+    {
+        console.log("HAHAHAH")
+    }
+
+}
+
+console.log(obj)
+
+let jsn=JSON.stringify(obj)
+
+console.log(jsn)
+
+// JSON.parse
+// TO reverse the effect converting the JSON to js object
+
+
+// localStorage
+
+localStorage.setItem()
+localStorage.getItem()
